38f18815694ff02161d06d92e49f97a8ee41b7ff
Gesang Paudra J <g.paudra@gmail.com>
2024-06-21 10:02:13 +0700

    Update DatabaseHelper.dart

--- lib/DatabaseHelper.dart
+++ lib/DatabaseHelper.dart
@@ -2,29 +2,29 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DatabaseHelper {
-  static final _databaseName = "banksampah.db";
-  static final _databaseVersion = 1;
+  static const _databaseName = "banksampah.db";
+  static const _databaseVersion = 1;
 
   // Table Anggota
-  static final tableAnggota = 'anggota';
-  static final columnId = 'id';
-  static final columnNama = 'nama';
-  static final columnAlamat = 'alamat';
-  static final columnNoTelepon = 'no_telepon';
+  static const tableAnggota = 'anggota';
+  static const columnId = 'id';
+  static const columnNama = 'nama';
+  static const columnAlamat = 'alamat';
+  static const columnNoTelepon = 'no_telepon';
 
   //Table Jenis Sampah
-  static final tableJenisSampah = 'jenis_sampah';
-  static final columnIdJenisSampah = 'id_jenis_sampah';
-  static final columnNamaJenisSampah = 'nama_jenis_sampah';
-  static final columnHargaJenisSampah = 'harga_jenis_sampah';
+  static const tableJenisSampah = 'jenis_sampah';
+  static const columnIdJenisSampah = 'id_jenis_sampah';
+  static const columnNamaJenisSampah = 'nama_jenis_sampah';
+  static const columnHargaJenisSampah = 'harga_jenis_sampah';
 
   //Table Transaksi
-  static final tableTransaksi = 'transaksi';
-  static final columnIdTransaksi = 'id_transaksi';
-  static final columnIdAnggota = 'id_anggota';
-  static final columnIdJenisSampahTransaksi = 'id_jenis_sampah_transaksi';
-  static final columnTanggalTransaksi = 'tanggal_transaksi';
-  static final columnJumlahTransaksi = 'jumlah_transaksi';
+  static const tableTransaksi = 'transaksi';
+  static const columnIdTransaksi = 'id_transaksi';
+  static const columnIdAnggota = 'id_anggota';
+  static const columnIdJenisSampahTransaksi = 'id_jenis_sampah_transaksi';
+  static const columnTanggalTransaksi = 'tanggal_transaksi';
+  static const columnJumlahTransaksi = 'jumlah_transaksi';
 
 DatabaseHelper._privateConstructor();
 static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
