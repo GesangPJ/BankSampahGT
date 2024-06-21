34ae2a9a2d25555bddd83661a0addaa2065368f3
Gesang Paudra J <g.paudra@gmail.com>
2024-06-21 20:52:00 +0700

    update name

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class BankSampahGT extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Bank Sampah Kemuning'),
+      home: const MyHomePage(title: 'Bank Sampah GT'),
     );
   }
 }
